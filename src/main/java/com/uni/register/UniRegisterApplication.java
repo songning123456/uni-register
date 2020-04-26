19521a20ba81189750787ce5773acb4db2a8590d
songning <1457065857@qq.com>
2020-04-26 17:03:55 +0800

    first commit

---
+++ src/main/java/com/uni/register/UniRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.uni.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniRegisterApplication.class, args);
+    }
+
+}
