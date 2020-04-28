a28bb9965582c2e2b21e88299ba083a2835cfc3e
songning <1457065857@qq.com>
2020-04-28 23:01:21 +0800

    zookeeper publish

--- src/main/java/com/uni/register/starter/RegisterApplicationRunner.java
+++ src/main/java/com/uni/register/starter/RegisterApplicationRunner.java
@@ -2,11 +2,13 @@ package com.uni.register.starter;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.uni.register.dao.RoutersDao;
 import com.uni.register.dto.RoutersDTO;
 import com.uni.register.entity.RoutersEntity;
 import com.uni.register.repository.RoutersRepository;
 import com.uni.register.util.ClassConvertUtil;
 import com.uni.register.zookeeper.ZKCuratorClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -21,22 +23,19 @@ import java.util.List;
  * description
  */
 @Component
+@Slf4j
 public class RegisterApplicationRunner implements ApplicationRunner {
 
     @Autowired
-    private RoutersRepository routersRepository;
-    @Autowired
-    private ZKCuratorClient zkCuratorClient;
+    private RoutersDao routersDao;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        this.sendZkRouters();
-    }
-
-    private void sendZkRouters() {
-        List<RoutersEntity> src = routersRepository.findAll();
-        List<RoutersDTO> target = new ArrayList<>();
-        ClassConvertUtil.populateList(src, target, RoutersDTO.class);
-        zkCuratorClient.sendOperator("uni-register", JSON.toJSONString(target));
+    public void run(ApplicationArguments args) {
+        try {
+            routersDao.sendZkRouters();
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("发送Zookeeper Routers fail: {}", e.getMessage());
+        }
     }
 }
--- src/test/java/com/uni/register/UniRegisterApplicationTests.java
+++ src/test/java/com/uni/register/UniRegisterApplicationTests.java
@@ -1,14 +1,21 @@
 package com.uni.register;
 
+import com.alibaba.fastjson.JSON;
+import com.uni.register.dao.RoutersDao;
+import com.uni.register.dto.RoutersDTO;
 import com.uni.register.entity.RoutersEntity;
 import com.uni.register.repository.RoutersRepository;
+import com.uni.register.util.ClassConvertUtil;
+import com.uni.register.zookeeper.ZKCuratorClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +23,8 @@ public class UniRegisterApplicationTests {
 
     @Autowired
     private RoutersRepository routersRepository;
+    @Autowired
+    private RoutersDao routersDao;
 
     @Test
     public void contextLoads() {
@@ -27,18 +36,6 @@ public class UniRegisterApplicationTests {
         entity.setUrl("/uni-register/hello/say");
         entity.setIpPort("127.0.0.1:7082");
         routersRepository.save(entity);
+        routersDao.sendZkRouters();
     }
-
-    @Test
-    public void contextLoads2() {
-        RoutersEntity entity = new RoutersEntity();
-        entity.setName("注册中心");
-        entity.setAuthority(false);
-        entity.setCreateTime(new Date());
-        entity.setDescription("获取注册中心");
-        entity.setUrl("/uni-register/routers/findRouter");
-        entity.setIpPort("127.0.0.1:7082");
-        routersRepository.save(entity);
-    }
-
 }
