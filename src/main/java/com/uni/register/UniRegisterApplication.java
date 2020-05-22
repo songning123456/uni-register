d5050f0ec36abb1fd90b24786f79c627e2eb4457
songning <1457065857@qq.com>
2020-05-22 13:33:53 +0800

    spring cloud gateway + eureka

--- src/main/java/com/uni/register/UniRegisterApplication.java
+++ src/main/java/com/uni/register/UniRegisterApplication.java
@@ -2,8 +2,13 @@ package com.uni.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author songning
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class UniRegisterApplication {
 
     public static void main(String[] args) {
